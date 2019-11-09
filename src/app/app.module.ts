7587d4c26b79b1f29f33751162190fa4bb500071
Samuel <samueltos@yahoo.com>
2019-11-09 13:14:36 -0500

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormioAppConfig } from 'angular-formio';
+import { FormioAuthService, FormioAuthConfig } from 'angular-formio/auth';
+import { AuthConfig, AppConfig } from '../config';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -23,10 +28,18 @@ import { HomeComponent } from './home/home.component';
       {
         path: 'home',
         component: HomeComponent
+      },
+      {
+        path: 'auth',
+        loadChildren: './auth/auth.module#AuthModule'
       }
     ])
   ],
-  providers: [],
+  providers: [
+    FormioAuthService,
+    {provide: FormioAuthConfig, useValue: AuthConfig},
+    {provide: FormioAppConfig, useValue: AppConfig}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
